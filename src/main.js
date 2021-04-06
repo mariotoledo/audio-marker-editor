1744d17d05b35489276aeddbcbf5b7c9a361958b
Mario Toledo <mario.toledo@wildlifestudios.com>
2021-04-06 16:55:02 -0300

    adding project with vue-audio-visual

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import AudioVisual from 'vue-audio-visual'
+
+Vue.use(AudioVisual)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
