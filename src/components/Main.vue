09a24df6894348f95e1e40ac2e37f8257c36d958
Mario Toledo <mario.toledo@wildlifestudios.com>
2021-04-07 00:37:11 -0300

    adding button to add marker

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,16 @@
 <template>
-  <av-waveform
-    audio-src="/static/example.mp3">
-  </av-waveform>
+  <div>
+    <av-waveform audio-src="/static/example.mp3"></av-waveform>
+    <button v-on:click="addMarker" class="btn btn-primary btn-block btn-add-marker"> Add Marker </button>
+    <div class="row" v-for="marker in markers" :key="marker.time">
+      <div class="col-md-5">
+        <div class="form-group">
+          <label for="formGroupExampleInput">Time</label>
+          <input v-model="marker.time" type="time" step="0.001" class="form-control">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +18,21 @@ export default {
   name: 'Main',
   data () {
     return {
-      file: null
+      markers: []
+    }
+  },
+  methods: {
+    addMarker: function() {
+      console.log('jdksfklsf');
+      let audio = document.getElementsByTagName('audio');
+      console.log('audio[0].currentTime', audio[0].currentTime);
+      this.markers.push({
+        time: this.msToTime(audio[0].currentTime * 1000),
+        type: 'normal'
+      })
+    },
+    msToTime: function(s) {
+      return new Date(s).toISOString().slice(11, -1);
     }
   }
 }
@@ -30,5 +53,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.btn-add-marker {
+  margin: 20px 0px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import App from './App'
 
 import AudioVisual from 'vue-audio-visual'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.use(AudioVisual)
 
 Vue.config.productionTip = false
