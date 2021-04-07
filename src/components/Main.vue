307750640b2612d0ad146b0a04a3c5465e2f9da2
Mario Toledo <mario.toledo@wildlifestudios.com>
2021-04-07 12:28:11 -0300

    generate json button

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,14 +2,35 @@
   <div>
     <av-waveform audio-src="/static/example.mp3"></av-waveform>
     <button v-on:click="addMarker" class="btn btn-primary btn-block btn-add-marker"> Add Marker </button>
-    <div class="row" v-for="marker in markers" :key="marker.time">
+    <div class="row" v-for="(marker, index) in markers" :key="marker.time">
       <div class="col-md-5">
         <div class="form-group">
-          <label for="formGroupExampleInput">Time</label>
+          <label>Time</label>
           <input v-model="marker.time" type="time" step="0.001" class="form-control">
         </div>
       </div>
+      <div class="col-md-5">
+        <div class="form-group">
+          <label>Label</label>
+          <input v-model="marker.label" type="text" class="form-control">
+        </div>
+      </div>
+      <div class="col-md-2">
+        <button class="btn btn-block btn-danger btn-remove" v-on:click="removeMarker(index)"><b-icon icon="trash"></b-icon></button>
+      </div>
     </div>
+    <button v-b-modal="'bv-json-modal'" class="btn btn-block btn-primary btn-generate-json">Generate JSON</button>
+    <b-modal id="bv-json-modal" hide-footer>
+      <template #json-title>
+        Generated Code
+      </template>
+      <div class="d-block text-center">
+        <pre v-html="this.markers">
+          {{markers}}
+        </pre>
+      </div>
+      <b-button class="mt-3" block @click="$bvModal.hide('bv-json-modal')">Close Me</b-button>
+    </b-modal>
   </div>
 </template>
 
@@ -23,13 +44,16 @@ export default {
   },
   methods: {
     addMarker: function() {
-      console.log('jdksfklsf');
       let audio = document.getElementsByTagName('audio');
       console.log('audio[0].currentTime', audio[0].currentTime);
       this.markers.push({
         time: this.msToTime(audio[0].currentTime * 1000),
-        type: 'normal'
+        msTime: audio[0].currentTime,
+        label: ''
       })
+    },
+    removeMarker: function (index) {
+      this.markers.splice(index, 1);
     },
     msToTime: function(s) {
       return new Date(s).toISOString().slice(11, -1);
@@ -56,5 +80,14 @@ a {
 }
 .btn-add-marker {
   margin: 20px 0px;
+}
+.btn-remove {
+  margin-top: 2rem;
+}
+pre {
+  text-align: left;
+}
+.btn-generate-json {
+  margin-bottom: 20px;
 }
 </style>
